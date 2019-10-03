15caba2be839fedf62cc8fc4995c37cdffa3d101
Tim Van de Cruys <timvdc@gmail.com>
2019-10-03 12:05:29 +0200

    add corpusreader for frog

---
+++ corpusreader/frogcorpusreader.py
@@ -0,0 +1,26 @@
+from .corpusreader import *
+import re
+
+posMatch = re.compile(r'([A-Z]+)\(.*')
+
+class FrogCorpusReader(CorpusReader):
+    def __init__(self, fileids):
+        CorpusReader.__init__(self, fileids)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        sentenceList = []
+        while True:
+            line = self.fileStream.next()
+            line = line.rstrip()
+            if not line:
+                return sentenceList
+            else:
+                tokenFields = line.split('\t')
+                lemma = tokenFields[2]
+                pos = tokenFields[4]
+                posMatcher = posMatch.match(pos)
+                broadPos = posMatcher.group(1)
+                sentenceList.append(lemma + '/' + broadPos)
