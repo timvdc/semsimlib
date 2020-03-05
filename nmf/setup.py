2321a24c667eee91c554d4a6c11f9214b8b9c7cf
Tim Van de Cruys <ego@timvandecruys.be>
2020-03-05 16:46:53 +0100

    update cython setup file

--- nmf/setup.py
+++ nmf/setup.py
@@ -1,7 +1,9 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
     name = "nmf",
     ext_modules = cythonize('nmf.pyx'), # accepts a glob pattern
+    include_dirs=[numpy.get_include()]
 )
