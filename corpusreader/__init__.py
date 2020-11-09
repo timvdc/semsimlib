fcfd84533951a18a4626b7de9102e1b0307d6c22
Tim Van de Cruys <timvdc@gmail.com>
2020-11-09 19:55:58 +0100

    corpusreader corrections

--- corpusreader/__init__.py
+++ corpusreader/__init__.py
@@ -1,4 +1,4 @@
 from .dependencycorpusreader import *
-from .plaintextcorpusreader import *
-from .ukwaccorpusreader import *
 from .frogcorpusreader import *
+from .plaintextcorpusreader import *
+from .poscorpusreader import *
